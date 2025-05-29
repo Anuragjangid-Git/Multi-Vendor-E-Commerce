16f68200e7b9208dd46694edd5b6c36c82b2a187
Anuragjangid-Git <anuragjangid62@gmail.com>
2025-05-29 11:50:58 +0530

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
-
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-       <p className="text-green-500 text-4xl">Anurag Jangid</p>
+    <div className="flex flex-col gap-y-4 m-4">
+      <div>
+        <Button variant={"elevated"}>Hello</Button>
+      </div>
+      <div>
+        <Input placeholder="Hellooo" />
+      </div>
+      <div>
+        <Progress value={67} />
+      </div>
+      <div>
+        <Textarea placeholder="Hellooooo" />
+      </div>
     </div>
   );
 }
